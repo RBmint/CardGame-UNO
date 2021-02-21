074af4f1cb9808701e905caec2d24208540e4d59
RBmint <mzhai4@illinois.edu>
2021-02-21 11:12:34 -0600

    updated javadoc for player class

--- UNOGame/src/Main/Player.java
+++ UNOGame/src/Main/Player.java
@@ -20,33 +20,82 @@ public class Player {
         myCards = new LinkedList<>();
     }
 
+    /**
+     * Switch the player's turn to play (or not).
+     */
     public void switchTurn() {
         isTurnToPlay = !isTurnToPlay;
     }
+
+    /**
+     * Check if it is the current player's turn to play.
+     * @return True if it is, false otherwise.
+     */
     public boolean isTurnToPlay() {
         return isTurnToPlay;
     }
+
+    /**
+     * Check if player has this particular card in his hand.
+     * @param thisCard the card to be checked
+     * @return True if the player has it, false otherwise.
+     */
     public boolean hasThisCard(UNOCard thisCard) {
         return myCards.contains(thisCard);
     }
+
+    /**
+     * Remove a particular card from the player's hand.
+     * @param thisCard the card to be removed.
+     */
     public void removeCardFromHand(UNOCard thisCard) {
         myCards.remove(thisCard);
     }
+
+    /**
+     * Get the name of the player.
+     * @return name of the player.
+     */
     public String getPlayerName() {
         return playerName;
     }
+
+    /**
+     * Add a particular card to the player's hand.
+     * @param unoCard the card to be added.
+     */
     public void addCardToHand(UNOCard unoCard) {
         myCards.add(unoCard);
     }
+
+    /**
+     * Get the number of cards a player currently holds.
+     * @return The number of cards in hand.
+     */
     public int getNumCardInHand() {
         return myCards.size();
     }
+
+    /**
+     * Check if a player has any cards in hand at all.
+     * @return True if he has, false otherwise.
+     */
     public boolean hasCardInHand() {
         return !myCards.isEmpty();
     }
+
+    /**
+     * Get all cards in a player's hand.
+     * @return The list of cards in a player's hand.
+     */
     public LinkedList<UNOCard> getAllCards() {
         return myCards;
     }
+
+    /**
+     * Get the card that is the last drawn one from a player.
+     * @return the card that has been drawn most recently.
+     */
     public UNOCard getLastDrawnCard() {
         return myCards.getLast();
     }
